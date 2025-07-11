949e982f07bb8384512e3cd2dc0a15dfb3e1a644
Binnur KURT <deepcloudlabs@gmail.com>
2025-07-11 09:19:09 +0300

    initial commit

--- lottery-web-application/src/main/java/com/example/lottery/config/LotteryConfig.java
+++ lottery-web-application/src/main/java/com/example/lottery/config/LotteryConfig.java
@@ -1,6 +1,8 @@
 package com.example.lottery.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.ScopedProxyMode;
 
 /*
  lottery.max=1000
@@ -9,6 +11,7 @@ lottery.features.sorted=true
 lottery.features.unique=true
  **/
 @ConfigurationProperties(prefix = "lottery")
+@RefreshScope(proxyMode = ScopedProxyMode.NO)
 public record LotteryConfig(int max, int size, Features features) {
 	public record Features(boolean sorted, boolean unique) {
 	}
--- lottery-web-application/src/main/java/com/example/lottery/service/business/StandardLotteryService.java
+++ lottery-web-application/src/main/java/com/example/lottery/service/business/StandardLotteryService.java
@@ -3,23 +3,29 @@ package com.example.lottery.service.business;
 import java.util.List;
 import java.util.stream.IntStream;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
-import com.example.lottery.config.LotteryConfig;
 import com.example.lottery.dto.LotteryModel;
 import com.example.lottery.service.LotteryService;
 import com.example.lottery.service.RandomNumberService;
 
 @Service
+@RefreshScope
 public class StandardLotteryService implements LotteryService {
 
 	private final RandomNumberService randomNumberService;
-	private final LotteryConfig lotteryConfig;
+	private final int lotteryMax;
+	private final int lotterySize;
 	
-	public StandardLotteryService(RandomNumberService randomNumberService, LotteryConfig lotteryConfig) {
+	public StandardLotteryService(RandomNumberService randomNumberService, 
+			@Value("${lottery.max}") int lotteryMax, 
+			@Value("${lottery.size}") int lotterySize) {
 		this.randomNumberService = randomNumberService;
-		this.lotteryConfig = lotteryConfig;
-		System.err.println(lotteryConfig);
+		this.lotteryMax = lotteryMax;
+		this.lotterySize = lotterySize;
+		System.out.println("%d,%d".formatted(lotteryMax,lotterySize));
 	}
 	/*
 	private final List<RandomNumberService> randomNumberServices;
@@ -43,9 +49,9 @@ public class StandardLotteryService implements LotteryService {
 	public LotteryModel draw() {
 //	    var randomNumberService = randomNumberServices.get(counter.getAndIncrement()%randomNumberServices.size());
 		return new LotteryModel(
-				IntStream.generate(() -> randomNumberService.generate(1,lotteryConfig.max()))
+				IntStream.generate(() -> randomNumberService.generate(1,lotteryMax))
 		         .distinct()
-		         .limit(lotteryConfig.size())
+		         .limit(lotterySize)
 		         .sorted()
 		         .boxed()
 		         .toList());
