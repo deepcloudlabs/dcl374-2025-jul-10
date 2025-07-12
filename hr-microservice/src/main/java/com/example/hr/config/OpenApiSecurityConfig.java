d8547bbb0feeb727a326513c1030fdaad187839f
Binnur KURT <deepcloudlabs@gmail.com>
2025-07-12 16:09:54 +0300

    Create OpenApiSecurityConfig.java

---
+++ hr-microservice/src/main/java/com/example/hr/config/OpenApiSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.example.hr.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT"
+)
+@SecurityRequirement(name = "bearerAuth")
+public class OpenApiSecurityConfig {
+}
